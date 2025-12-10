b1f017798e3f5afaabe67dcb0912be673499396b
keshavbansal5766 <keshav22.dlp@gmail.com>
2025-12-10 23:39:22 +0530

    creating Dashboard, Home, Layout, Login, Preview, Resume pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Layout from "./pages/layout";
+import Dashboard from "./pages/Dashboard";
+import ResumeBuilder from "./pages/ResumeBuilder";
+import Preview from "./pages/Preview";
+import Login from "./pages/Login";
+
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="app" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="builder/:resumeId" element={<ResumeBuilder />} />
+        </Route>
+        <Route path="view/:resumeId" element={<Preview />} />
+         <Route path="login" element={<Login />} />
+      </Routes>
     </>
   );
 }
