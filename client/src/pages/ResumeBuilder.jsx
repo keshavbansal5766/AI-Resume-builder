1f6dd08ecdbbee8479d015687c96e5f501c792e5
keshavbansal5766 <keshav22.dlp@gmail.com>
2025-12-21 01:49:19 +0530

    creating persoanlInfoForm component and working on it

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -12,6 +12,7 @@ import {
   Sparkles,
   User,
 } from "lucide-react";
+import PersonalInfoForm from "../components/PersonalInfoForm";
 
 const ResumeBuilder = () => {
   const { resumeId } = useParams();
@@ -117,7 +118,19 @@ const ResumeBuilder = () => {
                 </div>
               </div>
               <div className="space-y-6">
-                {activeSection.id === "personal" && <div></div>}
+                {activeSection.id === "personal" && (
+                  <PersonalInfoForm
+                    data={resumeData.personal_info}
+                    onChange={(data) =>
+                      setResumeData((prev) => ({
+                        ...prev,
+                        personal_info: data,
+                      }))
+                    }
+                    removeBackground={removeBackground}
+                    setRemoveBackground={setRemoveBackground}
+                  />
+                )}
               </div>
             </div>
           </div>
