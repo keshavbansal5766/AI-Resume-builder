13f3305a99887b16573457c8d6eefcf54012a0bd
keshavbansal5766 <keshav22.dlp@gmail.com>
2025-12-24 22:53:25 +0530

    Created Experience form component to add experience in resume

--- client/src/components/ProfessionalSummaryForm.jsx
+++ client/src/components/ProfessionalSummaryForm.jsx
@@ -13,7 +13,7 @@ const ProfessionalSummaryForm = ({ data, onChange, setResumeData }) => {
             Add summary for your resume here
           </p>
         </div>
-        <button className="flex items-center gap-2 px-3 py-1 text-sm bg-purple-100  text-purple-700 rounded hover:bg-purple-200 transition-colors">
+        <button className="flex items-center gap-2 px-3 py-1 text-sm bg-purple-100  text-purple-700 rounded hover:bg-purple-200 transition-colors disabled:opacity-50">
           <Sparkle className="size-4" /> AI Enhance
         </button>
       </div>
