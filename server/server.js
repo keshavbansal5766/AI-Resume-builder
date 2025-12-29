df35e8204a08e6eb1fea4afc0112a7d0df118a20
keshavbansal5766 <keshav22.dlp@gmail.com>
2025-12-30 02:35:48 +0530

    create protectAuth and userRouter

--- server/server.js
+++ server/server.js
@@ -1,18 +1,21 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import connectDB from "./config/db.js";
+import connectDB from "./configs/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Database connection
-await connectDB()
+await connectDB();
 
 app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => res.send("Server is live"));
+app.use("/api/users", userRouter);
+
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
